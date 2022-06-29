07ef9ef16483b6c9a98c3f02014b484deee64d60
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2022-06-29 14:17:50 +0000

    Add helpful output to git setup script

--- git_config.sh
+++ git_config.sh
@@ -2,3 +2,9 @@
 
 git config --global user.name 'Isaac Vander Sluis'
 git config --global user.email isaacvandersluis@gmail.com
+
+echo 'Git config:'
+git config --list
+
+echo -e '\nCurrent commit:'
+git log --oneline -n1
